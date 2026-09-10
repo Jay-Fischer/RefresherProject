177c5fe0398a2bff00330ab222bea2c72bf5b01e
Jay-Fischer <jaysennfanning@gmail.com>
2026-09-10 17:26:58 -0500

    Doors Done

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    [SerializeField] Transform targetObject;
-    [SerializeField] float OpenSpeed = 2.0f;
-
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     public void OpenDoor(){
-        
+        animator.SetTrigger("Open");
     }
 
     public void CloseDoor(){
-        
+        animator.SetTrigger("Close");
     }
 
 
