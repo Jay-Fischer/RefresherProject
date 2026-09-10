614547cf6d7cdf03df7fd90c05785e4fc06a6310
Jay-Fischer <jaysennfanning@gmail.com>
2026-09-10 12:46:03 -0500

    Door Problems
    Saving Progress

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    [SerializeField] Transform targetObject;
+    [SerializeField] float OpenSpeed = 2.0f;
+
+    public void OpenDoor(){
+        
+    }
+
+    public void CloseDoor(){
+        
+    }
+
+
+}
